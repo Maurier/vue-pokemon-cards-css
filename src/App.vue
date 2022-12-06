d8c5a02ff2da7b8996f66c9c0be1ca9eab02eac0
Maurier <mauriel.ramirez@gmail.com>
2022-12-06 12:48:37 -0700

    group cards by effects

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="showcase" @click="(active = null)">
-    <card v-for="card of cards" @click.native.stop="(active = active === card ? null : card)" :key="card.id"
-      :name="card.name" :img="card.images.large" :number="card.number" :supertype="card.supertype"
-      :subtypes="card.subtypes" :rarity="card.rarity" :gallery="card.gallery" :active="(active === card)" />
+  <div class="showcase" @click="active = null">
+    <div v-for="(slice, index) of slices" :key="index">
+      <div class="slice">
+        <card
+          v-for="card of slice"
+          @click.native.stop="active = active === card ? null : card"
+          :key="card.id"
+          :name="card.name"
+          :img="card.images.large"
+          :number="card.number"
+          :supertype="card.supertype"
+          :subtypes="card.subtypes"
+          :rarity="card.rarity"
+          :gallery="card.gallery"
+          :active="active === card"
+        />
+      </div>
+      <hr />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Card from '@/components/card.vue';
+import Card from "@/components/card.vue";
 
-const cards = require('@/assets/data.json');
+let cards = require("@/assets/data.json");
 
-const transformedCards = cards.map((card) => {
+cards = cards.map((card) => {
   return {
     ...card,
     rarity: card.rarity.toLowerCase(),
@@ -20,27 +35,68 @@ const transformedCards = cards.map((card) => {
     subtypes: Array.isArray(card.subtypes)
       ? card.subtypes.join(" ").toLowerCase()
       : card.subtypes.toLowerCase(),
-    gallery: card.number.startsWith("TG")
-  }
-})
+    gallery: card.number.startsWith("TG"),
+  };
+});
 
 @Component({
   name: "app",
   components: {
-    Card
+    Card,
   },
 })
 export default class App extends Vue {
-  cards = transformedCards
-  active = null
+  active = null;
+  slices = [
+    // basics
+    cards.slice(0, 6),
+    // holos
+    cards.slice(6, 12),
+    // galaxies
+    cards.slice(12, 15),
+    // radiant
+    cards.slice(15, 18),
+    // basicGallery
+    cards.slice(60, 63),
+    // vee
+    cards.slice(18, 21),
+    // veeUltra
+    cards.slice(21, 24),
+    // veeAlt
+    [...cards.slice(27, 30), ...cards.slice(33, 36)],
+    // veeMax
+    cards.slice(24, 27),
+    // veeMaxAlt
+    [cards[36], cards[31], cards[37]],
+    // veeStar
+    cards.slice(39, 42),
+    // trainerHolo
+    cards.slice(42, 48),
+    // rainbow
+    cards.slice(48, 51),
+    // gold
+    cards.slice(51, 60),
+    // veeGallery
+    cards.slice(63, 69),
+  ];
 }
 </script>
 
 <style lang="scss" scoped>
 .showcase {
   padding: 4rem;
+}
+
+.slice {
+  margin-bottom: 4rem;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: 4rem;
+}
+
+hr {
+  opacity: 0.35;
+  border-bottom: 1px solid #000;
+  margin-bottom: 3rem;
 }
 </style>
